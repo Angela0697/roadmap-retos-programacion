d66fbe1d33378dd09d85b4944c1bca1979e11bfa
Angela0697 <angelalaverde97@gmail.com>
2025-03-03 16:43:32 -0500

    #00 JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Angela0697.js
@@ -0,0 +1,47 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario en una línea
+
+/*
+Comentario 
+de varias
+líneas
+*/
+
+// Variables
+var nombre = "Angela" //variable global
+let segundoNombre = "Patricia" //variable global
+
+// Constante
+const apellido = "Laverde"
+
+// Tipos de datos primitivos
+
+// String
+let cadenaTexto = "Hola"
+// Number
+let numeroEnteroM = 2025
+let numeroDecimal = 0.5
+// Booleano
+let booleano = false 
+// null
+let nulo = "nulo"
+// undefined
+let indefinido
+// Symbol
+let simbolo = Symbol("Este es un simbolo")
+// BigInt
+let númeroEnteroArbitrario = 7657584393938836465759n
+
+// Datos estructurados
+// object
+let persona = {
+  nombre: "Angela",
+  edad: 27,
+  mascota: "gato"
+}
+// array
+let edad = [16, 20, 13, 18, 19]
+
+// Imprimir por terminal
+console.log("¡Hola, JavaScript!")
